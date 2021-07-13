196710dd93dad253eb5ad111a6e8e10d3d430244
bereadyjoint01 <bereadyjoint01@yahoo.co.jp>
2021-07-13 13:41:57 +0000

    meshiterro実装

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '056e605d092757ae31a348d7649c75824689e907f88777a5f3366d7c6f6830b20afac190367e814cfddfe0addfc14ca8a334e9d2d6b1cdce61bfbe93b9247c30'
